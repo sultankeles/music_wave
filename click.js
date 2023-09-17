09fd0527a96484c05bd47a18d7986791cdab5159
sultankeles <sultankeles179@gmail.com>
2023-09-17 18:32:21 +0300

    music wave animation

---
+++ click.js
@@ -0,0 +1,10 @@
+alert("Click on screen and enjoy music !")
+
+var music = 
+
+document.getElementById("music")
+    music.load();
+document.addEventListener("click", function(){
+    music.play()
+})
+
